bf2cd0089b8efe12fdbdeaea3ceebbcf0ad5d0fa
Arthur V. M. Dantas <arthur.dantas@ifba.edu.br>
2024-08-20 20:37:41 -0300

    pedro

---
+++ calculos/basicos-soma.js
@@ -0,0 +1,41 @@
+/**
+ * Soma valores numéricos
+ * @param  {...number} numeros Valores a serem somados
+ * @returns Total da soma
+ */
+export function somarNumeros(...numeros) {
+  return numeros.reduce((total, proximo) => total + proximo, 0);
+}
+
+/**
+ * Soma valores numéricos presentes em um array
+ * @param {number} numeros Valores a serem somados
+ * @returns Total da soma
+ */
+export function somarNumerosArray(numeros) {
+  return numeros.reduce((total, proximo) => total + proximo, 0);
+}
+/**
+ * Soma, como valores numéricos, valores em strings
+ * @param  {...string} numeros Valores a serem somados
+ * @returns Total da soma
+ */
+export function somarStrings(...numeros) {
+  const valores = numeros.map((n) => Number(n));
+<<<<<<< HEAD:calculos/basicos-soma.js
+  return somarNumeros(valores);
+}
+
+/**
+ * Soma, como valores numéricos, valores presentes em um array de strings
+ * @param {string} numeros Valores a serem somados
+ * @returns Total da soma
+ */
+export function somarStringsArray(numeros) {
+  const valores = numeros.map((n) => Number(n));
+  return somarNumerosArray(valores);
+}
+=======
+  return somarNumerosArray(valores);
+}
+>>>>>>> ed9f8a9 (bug corrigido):calculos/basicos.js
